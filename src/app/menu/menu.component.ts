287317b9206f9e8b7c77fe7852dc5b03b40a5032
lokarithm <1385766+lokarithm@users.noreply.github.com>
2022-05-14 21:49:17 -0400

    Initial commit

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.less']
+})
+export class MenuComponent implements OnInit {
+  // @Input()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
